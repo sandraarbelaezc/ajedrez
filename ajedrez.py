e02aa0817a61375425b0082ea3703b00ff5d121c
sandraarbelaezc <sandraarbelaez@gmail.com>
2020-11-04 20:49:45 -0500

    Para esta versión, se agrega la clase ajedrez y se crea el método __init__  para inicializar los atributos

--- ajedrez.py
+++ ajedrez.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import numpy as np
+
+class ajedrez():
+
+    def __init__(self, pieza, x, y):
+        self.tablero = np.zeros((8,8))
+        self.pieza = pieza
+        self.x = x
+        self.y = y
+        self.tablero[x][y] = 8
