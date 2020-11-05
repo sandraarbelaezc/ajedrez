5138492ed6bf2356c18275f64a0b0e88a751584b
sandraarbelaezc <sandraarbelaez@gmail.com>
2020-11-04 20:51:39 -0500

    Para esta versión, se agrega los métodos que van a mostrar en pantalla las posiciones de las piezas y sus posibles movimientos

--- ajedrez.py
+++ ajedrez.py
@@ -11,3 +11,22 @@ class ajedrez():
         self.x = x
         self.y = y
         self.tablero[x][y] = 8
+    
+    def pintar_mov_1(self, movimientos):
+        posiciones = []
+        for i in movimientos:
+            if i[0] >=0 and i[1] >=0:
+                posiciones.append(i)
+        for i in posiciones:
+            self.tablero[i[0]][i[1]] = 1
+        return self.tablero
+    
+    def pintar_mov_2(self, movimientos):
+        posiciones = []
+        for i in movimientos:
+            for j in i:
+                if j[0] >=0 and j[1] >=0:
+                    posiciones.append(j)
+        for i in posiciones:
+            self.tablero[i[0]][i[1]] = 1
+        return self.tablero
